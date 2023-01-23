244e47862cb47da6391de261da8e351288a26e09
Nityanand Mathur <nityanandmathur@gmail.com>
2023-01-23 17:48:04 +0530

    Update Code

--- AffineCipher.java
+++ AffineCipher.java
@@ -3,65 +3,40 @@ import java.util.HashMap;
 public class AffineCipher {
     
     /** 
-     * @param a
-     * @param b
+     * @param key1
      * @return int
      */
-    public static int gcd(int a, int b) {
-        while(b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
+    public static int multiplicative_inverse(int key1)
+    {
+        for(int i = 0; i < 26; i++)
+            {
+                if((key1*i)%26 == 1)
+                    return i;
+            }
+        return 0;
     }
-
-    
-    /** 
-     * @param a
-     * @param m
-     * @return int
-     */
-    public static int findInverse(int a, int m) {
-        int m0 = m;
-        int y = 0, x = 1;
-        if (m == 1) return 0;
-        while (a > 1) {
-            int q = a / m;
-            int t = m;
-            m = a % m;
-            a = t;
-            t = y;
-            y = x - q * y;
-            x = t;
-        }
-        if (x < 0) x += m0;
-        return x;
-    }
-
     
     /** 
      * @param ciphertext
      */
     public static void affineBreak(String ciphertext) {
-        ciphertext = ciphertext.toUpperCase();
-        HashMap<Character, Integer> freqMap = new HashMap<>();
+        HashMap<Character, Integer> frequency = new HashMap<>();
 
         for(int i = 0; i < ciphertext.length(); i++) {
             char c = ciphertext.charAt(i);
-            if(freqMap.containsKey(c)) {
-                freqMap.put(c, freqMap.get(c) + 1);
+            if(frequency.containsKey(c)) {
+                frequency.put(c, frequency.get(c) + 1);
             } else {
-                freqMap.put(c, 1);
+                frequency.put(c, 1);
             }
         }
 
         char mostCommon = ' ';
         int mostCommonValue = 0;
-        for(char c : freqMap.keySet()) {
-            if(freqMap.get(c) > mostCommonValue) {
+        for(char c : frequency.keySet()) {
+            if(frequency.get(c) > mostCommonValue) {
                 mostCommon = c;
-                mostCommonValue = freqMap.get(c);
+                mostCommonValue = frequency.get(c);
             }
         }
 
@@ -69,13 +44,13 @@ public class AffineCipher {
         System.out.println("Value of most common letter: " + (mostCommon - 'A'));
 
         for(int a = 1; a < 26; a++) {
-            if(gcd(a, 26) == 1) {
+            if(a%2 != 0) {
                 int b = (mostCommonValue - (ciphertext.indexOf('E') - ciphertext.indexOf('A')) * a) % 26;
                 System.out.println("Possible values of a and b: " + a + " " + b);
                 String plaintext = "";
                 for(int i = 0; i < ciphertext.length(); i++) {
                     int c = (ciphertext.charAt(i) - 'A' - b);
-                    int inv = findInverse(a, 26);
+                    int inv = multiplicative_inverse(a);
                     plaintext += (char)((c * inv % 26 + 'A'));
                 }
                 System.out.println("Potential plaintext: " + plaintext);
@@ -83,7 +58,6 @@ public class AffineCipher {
         }
     }
 
-    
     /** 
      * @param args
      */
